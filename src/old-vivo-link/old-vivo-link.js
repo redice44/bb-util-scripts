47bf3922493ec84bdb40476122e18f8d90504ab3
Matt Thomson <red.cataclysm@gmail.com>
2017-02-02 13:41:02 -0500

    Converts to ES5

--- src/old-vivo-link/old-vivo-link.js
+++ src/old-vivo-link/old-vivo-link.js
@@ -7,16 +7,10 @@
 // @version      0.1.0
 // @description  Detects old vivo links
 // @author       Matt Thomson <red.cataclysm@gmail.com>
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.min.js
 // @match        https://fiu.blackboard.com/webapps/blackboard/content/listContentEditable.jsp?*
 // ==/UserScript==
 
-/* jshint ignore:start */
-var inline_src = (<><![CDATA[
-/* jshint ignore:end */
-/* jshint esnext: true */
-
+(function() {
 // Your code here...
 let links = document.querySelectorAll('#content_listContainer a');
 let items = [];
@@ -29,13 +23,9 @@ for (let l of links) {
   }
 }
 if (items.length > 0) {
-  alert (`There are ${items.length} old link(s) on this page highlighted in red.`);
+  alert ('There are ' + items.length + ' old link(s) on this page highlighted in red.');
   // List out items in console
   console.log(items);
 }
 
-/* jshint ignore:start */
-]]></>).toString();
-var c = babel.transform(inline_src);
-eval(c.code);
-/* jshint ignore:end */
+})();
