d20b679c5d01d3ca3821c7b3ebcb019081e35356
Matt Thomson <red.cataclysm@gmail.com>
2017-02-02 12:55:08 -0500

    Refactor: Removes unsafewindow

--- dist/chrome/replace-urls.user.js
+++ dist/chrome/replace-urls.user.js
@@ -7,47 +7,28 @@
 // @version      0.1.0
 // @description  Replaces all matching URLs in TinyMCE HTML content and Web Link URL field (if present) with new URL.
 // @author       Daniel Victoriano <victorianowebdesign@gmail.com>
-// @match        https://fiu.blackboard.com/*
-// @grant        unsafeWindow
+// @author       Matt Thomson <red.cataclysm@gmail.com>
+// @match        https://fiu.blackboard.com/webapps/blackboard/execute/manageCourseItem?*
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-if (unsafeWindow.tinyMCE !== undefined && document.body.className == "ineditmode") {
-  setTimeout(function() {
-
-    var oldUrl, newUrl, tinyMCE, tinyMCEId, tinyMCEHtml, webUrl, regex;
-
-    oldUrl = "fiuonline.mediasite.com";
-    newUrl = "fiuolmediasite.fiu.edu";
-
-    // Get Editor instance by ID
-    // Editor ID varies so get ID first
-    tinyMCEId = document.querySelector(".textboxtable textarea").id;
-    tinyMCE = unsafeWindow.tinyMCE.getInstanceById(tinyMCEId);
-
-    // Get TinyMCE HTML content
-    tinyMCEHtml = tinyMCE.getContent({format: "html"});
-
-    // Get URL field (if present)
-    webUrl = document.getElementById("url");
-
-    // Replace URLs
-    regex = new RegExp(oldUrl, "g");
-
-    if ( tinyMCEHtml.includes(oldUrl) ) {
-      tinyMCEHtml = tinyMCEHtml.replace(regex, newUrl);
-    }
-
-    if (webUrl !== null) {
-      if (webUrl.value.includes(oldUrl) ) {
-        var url = webUrl.value.replace(regex, newUrl);
-        webUrl.value = url;
-      }
-    }
-
-    tinyMCE.setContent(tinyMCEHtml);
-  }, 2100);
+function updateUrl(target, oldUrl, newUrl) {
+  return target.replace(new RegExp(oldUrl, 'g'), newUrl);
 }
+
+(function() {
+  'use strict';
+
+  var oldUrl = "fiuonline.mediasite.com";
+  var newUrl = "fiuolmediasite.fiu.edu";
+
+  var descriptionNode = document.getElementById('htmlData_text');
+  var urlNode = document.getElementById('url');
+
+  if (descriptionNode) {
+    descriptionNode.innerHTML = updateUrl(descriptionNode.innerHTML, oldUrl, newUrl);
+  }
+
+  if (urlNode) {
+    urlNode.value = updateUrl(urlNode.value, oldUrl, newUrl);
+  }
 })();
