8222b81a1110218d39e5ff6fd6f1c1d6b6851a86
Matt Thomson <red.cataclysm@gmail.com>
2017-02-02 13:47:37 -0500

    Firefox port

---
+++ dist/firefox/old-mediasites-link.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Old Mediasites Link Detector
+// @namespace    https://github.com/redice44
+// @source       https://github.com/redice44/bb-util-scripts/dist/chrome/old-mediasites-link.user.js
+// @updateURL    https://github.com/redice44/bb-util-scripts/dist/chrome/old-mediasites-link.user.js
+// @supportURL   https://github.com/redice44/bb-util-scripts/issues
+// @version      0.1.0
+// @description  Detects old Mediasites links
+// @author       Matt Thomson <red.cataclysm@gmail.com>
+// @match        https://fiu.blackboard.com/webapps/blackboard/content/listContentEditable.jsp?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+// Your code here...
+var links = document.querySelectorAll('#content_listContainer a');
+var items = [];
+var linkFlag = 'fiuonline.mediasite.com';
+
+for (var l of links) {
+  if (l.href.includes(linkFlag)) {
+    items.push(l.innerText);
+    l.setAttribute('style', 'background-color: #FF0000');
+  }
+}
+if (items.length > 0) {
+  alert ('There are ' + items.length + ' old link(s) on this page highlighted in red.');
+  // List out items in console
+  console.log(items);
+}
+
+})();
---
+++ dist/firefox/old-vivo-link.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Old ViVo Link Detector
+// @namespace    https://github.com/redice44
+// @source       https://github.com/redice44/bb-util-scripts/dist/chrome/old-vivo-link.user.js
+// @updateURL    https://github.com/redice44/bb-util-scripts/dist/chrome/old-vivo-link.user.js
+// @supportURL   https://github.com/redice44/bb-util-scripts/issues
+// @version      0.1.0
+// @description  Detects old vivo links
+// @author       Matt Thomson <red.cataclysm@gmail.com>
+// @match        https://fiu.blackboard.com/webapps/blackboard/content/listContentEditable.jsp?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+// Your code here...
+let links = document.querySelectorAll('#content_listContainer a');
+let items = [];
+let linkFlag = 'vivoId=';
+
+for (let l of links) {
+  if (l.href.includes(linkFlag)) {
+    items.push(l.innerText);
+    l.setAttribute('style', 'background-color: #FF0000');
+  }
+}
+if (items.length > 0) {
+  alert ('There are ' + items.length + ' old link(s) on this page highlighted in red.');
+  // List out items in console
+  console.log(items);
+}
+
+})();
