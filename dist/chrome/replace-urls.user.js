535929afe066d590c0f5085a34ebfba69ed05b8f
Matt Thomson <red.cataclysm@gmail.com>
2017-02-02 13:16:52 -0500

    Handles assignments and assessments

--- dist/chrome/replace-urls.user.js
+++ dist/chrome/replace-urls.user.js
@@ -9,10 +9,39 @@
 // @author       Daniel Victoriano <victorianowebdesign@gmail.com>
 // @author       Matt Thomson <red.cataclysm@gmail.com>
 // @match        https://fiu.blackboard.com/webapps/blackboard/execute/manageCourseItem?*
+// @match        https://fiu.blackboard.com/webapps/assessment/do/content/assessment?*
+// @match        https://fiu.blackboard.com/webapps/assessment/do/authoring/modifyAssessment*
+// @match        https://fiu.blackboard.com/webapps/assignment/execute/manageAssignment?*
 // ==/UserScript==
 
 function updateUrl(target, oldUrl, newUrl) {
   return target.replace(new RegExp(oldUrl, 'g'), newUrl);
+}
+
+function getEditNodes() {
+  var id = {
+    'item': 'htmlData_text',                        // Items, weblinks, etc
+    'weblink': 'url',                               // Weblinks
+    'assignment': 'content_desc_text',              // Assignments
+    'assessment': 'descriptiontext',                // Test Options
+    'assessmentInstructions': 'instructionstext'    // Editing Test
+  };
+
+  var url = window.location.href;
+
+  if (url.includes('/webapps/blackboard/execute/manageCourseItem?')) {
+    // Item, weblink, etc.
+    return [id.item, id.weblink];
+  }
+
+  if (url.includes('/webapps/assessment/do/')) {
+    // Assessments: Edit Test or Edit Options
+    return [id.assessment, id.assessmentInstructions];
+  }
+
+  if (url.includes('/webapps/assignment/execute/manageAssignment?')) {
+    return [id.assignment];
+  }
 }
 
 (function() {
@@ -21,14 +50,21 @@ function updateUrl(target, oldUrl, newUrl) {
   var oldUrl = "fiuonline.mediasite.com";
   var newUrl = "fiuolmediasite.fiu.edu";
 
-  var descriptionNode = document.getElementById('htmlData_text');
-  var urlNode = document.getElementById('url');
+  var nodes = getEditNodes();
 
-  if (descriptionNode) {
-    descriptionNode.innerHTML = updateUrl(descriptionNode.innerHTML, oldUrl, newUrl);
-  }
+  nodes = nodes.map(function(id) {
+    return document.getElementById(id);
+  });
 
-  if (urlNode) {
-    urlNode.value = updateUrl(urlNode.value, oldUrl, newUrl);
-  }
+  nodes.forEach(function(node) {
+    if (node) {
+      if (node.nodeName.toLowerCase() === 'input') {
+        node.value = updateUrl(node.value, oldUrl, newUrl);
+      } else if (node.nodeName.toLowerCase() === 'textarea') {
+        node.innerHTML = updateUrl(node.innerHTML, oldUrl, newUrl);
+      } else {
+        console.log('Error: Unhandled node type.', node.nodeName);
+      }
+    }
+  });
 })();
