0d6c9f2637ef40b8e5f7ec6907295b53c91ae3a9
Matt Thomson <red.cataclysm@gmail.com>
2017-02-07 11:09:33 -0500

    Testing scope

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,16 +1,16 @@
-function getFromStorage(key) {
+var getFromStorage = function (key) {
   // return JSON.parse(__storage__.getItem(key));
   // console.log('get: ', key,GM_getValue(key, null));
   // return JSON.parse(GM_getValue(key, null));
   return GM_getValue(key, null);
-}
+};
 
-function setToStorage(key, value) {
+var setToStorage = function (key, value) {
   // __storage__.setItem(key, JSON.stringify(value));
   GM_setValue(key, value);
-}
+};
 
-function delFromStorage(key) {
+var delFromStorage = function (key) {
   GM_deleteValue(key);
   // __storage__.removeItem(key);  
-}
+};
