3e782b3cd1dfeae1eab45ae1e530111e8fc21b16
Matt Thomson <red.cataclysm@gmail.com>
2017-02-10 08:21:18 -0500

    Merging item log into parser

--- src/dom/parsePage.js
+++ src/dom/parsePage.js
@@ -5,12 +5,78 @@ var parsePage = function (plugins) {
 
   contentItems.forEach(function(item) {
     page = directoryPlugin.parser(item, page);
+    page = logItemPlugin.parser(item, page);
     plugins.forEach(function(plugin) {
       page = plugin.parser(item, page);
     });
   });
 
   return page;
+};
+
+
+var logItemPlugin = {
+  parser: function (item, page) {
+    if (!page.hasOwnProperty('items')) {
+      page = Object.assign({}, { items: [] }, page);
+    }
+
+    page.items.push(this.__makeItem__(item));
+    return page;
+  },
+  getDom: function(item) {
+    if (item.items && item.items.length > 0) {
+      var list = document.createElement('ol');
+      item.items.forEach(function(i) {
+        var domNode = document.createElement('li');
+        var title = i.title;
+        domNode.appendChild(document.createTextNode(title));
+        list.appendChild(domNode);
+      });
+
+      return list;
+    }
+
+    var domNode = document.createElement('p');
+    domNode.appendChild(document.createTextNode('Empty'));
+
+    return domNode;
+  },
+  __makeItem__: function(item) {
+    var contentFolderController = '/webapps/blackboard/content/listContentEditable.jsp?';
+
+    var itemResult = {};
+    var link = item.querySelector('div.item > h3 > a');
+
+
+    if (link && link.href.includes(contentFolderController)) {
+      itemResult = Object.assign({}, this.__getLink__(link));
+    } else {
+      // Not a folder
+      itemResult.title = item.querySelector('div.item > h3').innerText;
+    }
+
+    return itemResult;
+  },
+  __getLink__: function (link) {
+    var contentId = 'Error: Content ID not parsed';
+    var courseId = 'Error: Course ID not parsed';
+    var params = getParameters(link.href);
+
+    if (params.hasOwnProperty('content_id')) {
+      contentId = params.content_id;
+    }
+    if (params.hasOwnProperty('course_id')) {
+      courseId = params.course_id;
+    }
+
+    return {
+      url: link.href,
+      title: link.innerText,
+      contentId: contentId,
+      courseId: courseId
+    };
+  }
 };
 
 var directoryPlugin = {
