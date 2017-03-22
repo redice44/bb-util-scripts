9442b669a6ba60e30aba221630f26d6bf304e97f
Matt Thomson <red.cataclysm@gmail.com>
2017-03-22 15:32:17 -0400

    Build System Update

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    // courseScanner: './src/course-scan/course-scanner.js',
-    test: './test.js'
+    // nameOfOutputFile: './path/to/file.js'
+    CourseScanner: './src/scripts/CourseScanner.js'
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].user.js'
   },
   resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'src')]
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src', 'lib'),
+      path.resolve(__dirname, 'src', 'interface')
+    ]
   }
 };
