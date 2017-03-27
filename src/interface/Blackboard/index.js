9b6ded9daa6a81a224577ff46ebc5409889b67a5
Matt Thomson <red.cataclysm@gmail.com>
2017-03-27 16:10:36 -0400

    Blackboard Interface

--- src/interface/Blackboard/index.js
+++ src/interface/Blackboard/index.js
@@ -12,6 +12,13 @@ function BlackboardInterface(domain) {
     courseMenu: 'courseMenuPalette_contents',
     contentItems: 'content_listContainer'
   };
+
+  this.q = {
+    courseMenuLink: 'li.clearfix > a',    // Course Menu Link
+    contentItems: 'li.liItem',            // Content Items
+    itemLink: 'div.item > h3 > a'         // Content Item Link
+  };
+
   this.endpoints = {
     courseLauncher: '/webapps/blackboard/execute/launcher?',
     contentFolder: '/webapps/blackboard/content/listContentEditable.jsp?'
@@ -22,45 +29,34 @@ function BlackboardInterface(domain) {
 BlackboardInterface.prototype = Object.create(DOMInterface.prototype);
 BlackboardInterface.prototype.constructor = BlackboardInterface;
 
-/**
-  @param {string} id - Course's Id
-  @return {DOM Node} - DOM of the course's main page.
-*/
-BlackboardInterface.prototype.getCourse = function (id) {
-  var that = this;
-  request
-    .get(`${domain}${this.endpoints.courseLauncher}type=Course&id=${id}&url=`)
-    .end(function (err, res) {
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(res.text, "text/html");
-      var items = that.getChildren(that.getId(doc, that.ids.contentItems), 'li.liItem');
-      console.log(items);
-      // dump dom into 'return'
-    });
-  // request page
-  // parse doc
-  // return doc
-};
-
 /**  
   @param {string} id - Course Id
-  @return {Page[]} List of top level pages.
+  @return {Promise} On resolve: Page[]
 */
 BlackboardInterface.prototype.getTopLevel = function (id) {
-  var doc = this.getCourse(id);
-  this.updateDoc(doc);
-  var menuItems = this
-    .getChildren(this.getId(this.ids.courseMenu), 'li.clearfix > a')
-    .filter(function (link) {
-      return link.href.includes(this.endpoints.contentFolder);
-    });
+  var that = this;
+  return new Promise(function (resolve, reject) {
+    request
+      .get(`${that.domain}${that.endpoints.courseLauncher}type=Course&id=${id}&url=`)
+      .end(function (err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          var parser = new DOMParser();
+          var doc = parser.parseFromString(res.text, "text/html");
+          var links = that.getChildren(that.getId(doc, that.ids.courseMenu), that.q.courseMenuLink)
+            .filter(function (link) {
+              return that.getUrl(link).includes(that.endpoints.contentFolder);
+            });
+          var pages = links.map(function (link) {
+            var p = getParameters(that.getUrl(link));
+            return new Page(id, p.content_id);
+          });
 
-  menuItems = menuItems.map(function (item) {
-    var p = getParameters(item);
-    return new Page(p.content_id);
+          resolve(pages);
+        }
+      });
   });
-
-  return menuItems;
 };
 
 /**
@@ -68,9 +64,36 @@ BlackboardInterface.prototype.getTopLevel = function (id) {
   @return: {Page} Contains items on page (shallow)
 */
 BlackboardInterface.prototype.getPage = function (page) {
-  // request page from bb
-  // parse out items
-  // return page (shallow)
+  var that = this;
+  return new Promise(function (resolve, reject) {
+    request
+      .get(`${that.domain}${that.endpoints.contentFolder}content_id=${page.id}&course_id=${page.courseId}`)
+      .end(function (err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          var parser = new DOMParser();
+          var doc = parser.parseFromString(res.text, "text/html");
+          var items = that.getChildren(that.getId(doc, that.ids.contentItems), that.q.contentItems);
+          if (items) {
+            items.forEach(function (item) {
+              var contentId = that.getContentId(item);
+              if (that.isPage(item)) {
+                page.addItem(new Page(page.courseId, contentId));
+              } else {
+                page.addItem(new Item(page.courseId, contentId));
+              }
+            });
+          }
+          
+          resolve(page);
+        }
+      });
+  });
+};
+
+BlackboardInterface.prototype.getContentId = function (item) {
+  return this.getChild(item, 'div.item').getAttribute('id');
 };
 
 /**
@@ -78,7 +101,14 @@ BlackboardInterface.prototype.getPage = function (page) {
   @return {boolean}
 */
 BlackboardInterface.prototype.isPage = function (item) {
-  // validation
+  var href = this.getUrl(this.getChild(item, this.q.itemLink));
+
+  if (href) {
+    return href.includes(this.endpoints.contentFolder);
+  }
+  return false;
 };
+
+
 
 export default BlackboardInterface;
